fd6cf1c1f2981f7c28035da2b3e43ce0b6da1291
Bruno Tavares <eu@brunotavares.net>
2013-01-16 14:44:27 +0000

    Directory support

--- pendium.py
+++ pendium.py
@@ -2,43 +2,41 @@ import os
 import markdown
 
 from config import Config
-from flask  import Flask
-from flask  import Markup
-from flask  import render_template
+from flask  import Flask, Markup, render_template
+
+config = None
 
 class Pendium:
-    def __init__(self, filename = None):
-        self.cfg = Config( file( 'pendium.cfg' ) )
+    def __init__(self, path):
+        self.path     = path
+        self.abs_path = os.path.join(config.wiki_dir, path)
+        self.name     = os.path.split(self.abs_path)[1] 
+        self.is_node  = False
+        self.is_leaf  = False
 
-        if filename != None:
-            self.filename = filename
+        if os.path.isdir( self.abs_path ):
+            self.is_node = True
+        else:
+            self.is_leaf = True
 
-
-    def get_md_files(self):
+    def items(self):
         filenames = []
-        for filename in os.listdir( self.cfg.wiki_dir ):
-            if filename[ filename.find('.') + 1: ] not in self.cfg.extensions:
-                continue
+        if not os.path.isdir(self.abs_path):
+            return filenames
+             
+        for f in os.listdir( self.abs_path ):
+            complete_abs_path = os.path.join( self.abs_path, f )
+            complete_path     = os.path.join( self.path, f )
+            if os.path.isdir( complete_abs_path ):
+                filenames.append( Pendium( complete_path ) )
 
-            filenames.append( Pendium(filename) )
+            elif f[ f.find('.') + 1: ] in config.extensions:
+                filenames.append( Pendium( complete_path ) )
 
         return filenames
 
-
-    def short_filename( self ):
-        return self.filename.replace( '.', '_' )
-
-
-    # This sucks
-    def unshort_filename( self ):
-        filename = self.filename[::-1]
-        filename = filename.replace( '_', '.', 1 )
-        return filename[::-1]
-
-
     def get_md_file( self ):
-        complete_filename = '/'.join( [ self.cfg.wiki_dir,
-                                        self.unshort_filename() ] )
+        complete_filename = self.abs_path
 
         try:
             f = open( complete_filename, 'r' )
@@ -46,36 +44,38 @@ class Pendium:
             return "File not found."
 
         markdown_content = f.read().decode('utf-8')
-        markdown_content = markdown.markdown( markdown_content, self.cfg.markdown_plugins )
+        markdown_content = markdown.markdown( markdown_content, config.markdown_plugins )
 
         return Markup( markdown_content )
-
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    p = Pendium()
-    return render_template( 'index.html', files= p.get_md_files() )
+    p = Pendium( '.' )
+    return render_template( 'index.html', files=p.items() )
 
 
-@app.route('/<filename>')
-def view( filename ):
-    p = Pendium(filename)
-    md_html = p.get_md_file()
+@app.route('/<path:path>')
+def view( path ):
+    p = Pendium( path )
 
-    return render_template( 'view.html', filename = filename,
-                                         files    = p.get_md_files(),
-                                         markdown = md_html
-                          )
+    if p.is_leaf:
+        md_html = p.get_md_file()
 
+        return render_template( 'view.html',  file     = p,
+                                              files    = p.items(),
+                                              markdown = md_html
+                              )
+    elif p.is_node:
+        return render_template( 'list.html', files=p.items(), file=p )
 
 if __name__ == '__main__':
-    pendium = Pendium()
+    config = Config( file( 'pendium.cfg' ) )
 
-    if pendium.cfg.host_ip: # Run server externally
+    if config.host_ip: # Run server externally
         app.debug = False
-        app.run( host = pendium.cfg.host_ip )
+        app.run( host = config.host_ip )
 
     app.debug = True
     app.run()
